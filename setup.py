4608a75c39bf9a2590bb7aec6b9104b7e08d95cc
Pengfei Liu <59123869+pfliu-nlp@users.noreply.github.com>
2021-06-02 09:25:57 -0400

    update license format

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
   "Intended Audience :: Developers",
   "Topic :: Text Processing",
   "Topic :: Scientific/Engineering :: Artificial Intelligence",
-  "License :: MIT License",
+  "License :: OSI Approved :: MIT License",
   "Programming Language :: Python :: 3",
   ],
   packages=find_packages(),
