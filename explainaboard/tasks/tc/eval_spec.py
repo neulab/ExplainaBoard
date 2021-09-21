ff94bee95fecdfbc88523b0b1aa92e7c7e80fbc6
Graham Neubig <neubig@gmail.com>
2021-09-20 22:41:42 -0400

    Dedup ECE calculation
    Former-commit-id: 0adc8ed6ec70ae00c79ae3b29df73035a7c2f613 [formerly f2c4dc4943ca2194d5597f84e133c7ff58f7e1fe]
    Former-commit-id: 8a4d64766a9676d507e68d979c1a3eccd10304f1

--- explainaboard/tasks/tc/eval_spec.py
+++ explainaboard/tasks/tc/eval_spec.py
@@ -48,57 +48,6 @@ def get_aspect_value(sent_list, sample_list_tag, sample_list_tag_pred, dict_aspe
     return dict_span2aspect_val, dict_span2aspect_val_pred, dict_sid2sent
 
 
-def process_all(file_path, size_of_bin=10, dataset='atis', model='lstm-self-attention'):
-    """
-
-    :param file_path: the file_path is the path to your file.
-
-    And the path must include file name.
-
-    the file name is in this format: test_dataset_model.tsv.
-
-    the file_path must in the format: /root/path/to/your/file/test_dataset.tsv
-
-    The file must in this format:
-    sentence\tground_truth\tpredict_label\tprobability\tright_or_not
-    if prediction is right, right_or_not is assigned to 1, otherwise 0.
-
-    :param size_of_bin: the numbers of how many bins
-
-    :param dataset: the name of the dataset
-
-    :param model: the name of the model
-
-    :return:
-    ece :the ece of this file
-    dic :the details of the ECE information in json format
-    """
-
-    # import json
-
-    probability_list, right_or_not_list = du.get_probability_right_or_not(file_path, prob_col=3, right_or_not_col=4)
-
-    raw_list = list(zip(probability_list, right_or_not_list))
-
-    bin_list = ea.divide_into_bin(size_of_bin, raw_list)
-
-    ece = ea.calculate_ece(bin_list)
-    dic = OrderedDict()
-    dic['dataset-name'] = dataset
-    dic['model-name'] = model
-    dic['ECE'] = ece
-    dic['details'] = []
-    basic_width = 1 / size_of_bin
-    for i in range(len(bin_list)):
-        tem_dic = {}
-
-        bin_name = format(i * basic_width, '.2g') + '-' + format((i + 1) * basic_width, '.2g')
-        tem_dic = {'interval': bin_name, 'average_accuracy': bin_list[i][1], 'average_confidence': bin_list[i][0],
-                   'samples_number_in_this_bin': bin_list[i][2]}
-        dic['details'].append(tem_dic)
-
-    return ece, dic
-
 def get_error_case(sent_list, true_label_list, pred_label_list):
     error_case_list = []
     for sent, true_label, pred_label in zip(sent_list, true_label_list, pred_label_list):
@@ -106,9 +55,10 @@ def get_error_case(sent_list, true_label_list, pred_label_list):
             error_case_list.append(true_label + "|||" + pred_label + "|||" + ea.format4json2(sent))
     return error_case_list
 
-def evaluate(task_type="ner", analysis_type="single", systems=[], dataset_name = 'dataset_name', model_name = 'model_name', output_filename="./output.json", is_print_ci=False,
-             is_print_case=False, is_print_ece=False):
 
+def evaluate(task_type="ner", analysis_type="single", systems=[], dataset_name='dataset_name', model_name='model_name',
+             output_filename="./output.json", is_print_ci=False,
+             is_print_case=False, is_print_ece=False):
     path_text = systems[0] if analysis_type == "single" else ""
     path_comb_output = "model_name" + "/" + path_text.split("/")[-1]
     dict_aspect_func, dict_precomputed_path, obj_json = ea.load_task_conf(task_dir=os.path.dirname(__file__))
@@ -127,7 +77,7 @@ def evaluate(task_type="ner", analysis_type="single", systems=[], dataset_name =
                                                                            n_times=1000)
 
     dict_span2aspect_val, dict_span2aspect_val_pred, dict_sid2sent = get_aspect_value(sent_list, true_label_list,
-                                                                                  pred_label_list, dict_aspect_func)
+                                                                                      pred_label_list, dict_aspect_func)
 
     holistic_performance = ea.accuracy(true_label_list, pred_label_list)
     holistic_performance = format(holistic_performance, '.3g')
@@ -150,8 +100,9 @@ def evaluate(task_type="ner", analysis_type="single", systems=[], dataset_name =
         dict_bucket2span_pred[aspect] = ea.bucket_attribute_specified_bucket_interval(dict_span2aspect_val_pred[aspect],
                                                                                       dict_bucket2span[aspect].keys())
         # dict_bucket2span_pred[aspect] = __select_bucketing_func(func[0], func[1], dict_span2aspect_val_pred[aspect])
-        dict_bucket2f1[aspect] = ea.get_bucket_acc_with_error_case(dict_bucket2span[aspect], dict_bucket2span_pred[aspect],
-                                                                dict_sid2sent, is_print_ci, is_print_case)
+        dict_bucket2f1[aspect] = ea.get_bucket_acc_with_error_case(dict_bucket2span[aspect],
+                                                                   dict_bucket2span_pred[aspect],
+                                                                   dict_sid2sent, is_print_ci, is_print_case)
         aspect_names.append(aspect)
     print("aspect_names: ", aspect_names)
 
@@ -190,8 +141,8 @@ def evaluate(task_type="ner", analysis_type="single", systems=[], dataset_name =
 
             # instantiation
             dict_fine_grained[aspect].append({"bucket_name": bucket_name, "bucket_value": bucket_value, "num": n_sample,
-                                             "confidence_low": confidence_low, "confidence_up": confidence_up,
-                                             "bucket_error_case": bucket_error_case})
+                                              "confidence_low": confidence_low, "confidence_up": confidence_up,
+                                              "bucket_error_case": bucket_error_case})
 
     obj_json["task"] = task_type
     obj_json["data"]["language"] = "English"
@@ -209,12 +160,11 @@ def evaluate(task_type="ner", analysis_type="single", systems=[], dataset_name =
     ece = 0
     dic_calibration = None
     if is_print_ece:
-        ece, dic_calibration = process_all(path_text,
-                                           size_of_bin=10, dataset="dataset_name", model="model_name")
+        ece, dic_calibration = ea.calculate_ece_by_file(path_text, prob_col=3, right_or_not_col=4,
+                                                        size_of_bin=10, dataset="dataset_name", model="model_name")
 
     obj_json["model"]["results"]["calibration"] = dic_calibration
     # print(dic_calibration)
-
 
     ea.save_json(obj_json, output_filename)
 
