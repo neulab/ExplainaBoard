6af8a84a6f8901afb058b419dbd93c0d1b74bb2b
Pengfei Liu <59123869+pfliu-nlp@users.noreply.github.com>
2022-02-04 11:50:30 -0500

    re-format/type overall performance in analysis report
    Former-commit-id: 60ecdae41c0a0461ae6ea6a7b27b8526f1926995 [formerly 1ad98cff7792f59c19fc59020597ef75c6598f93]
    Former-commit-id: 60f7f4011bcef5c54a0305cb78908cfa60c8629e

--- explainaboard/builders/extractive_qa.py
+++ explainaboard/builders/extractive_qa.py
@@ -124,16 +124,16 @@ class QASquadExplainaboardBuilder:
             confidence_score_low = 0
             confidence_score_up = 0
             overall_performance = Performance(
-                                                   metric_name=metric_name,
-                                                   value=format(overall_value, '.4g'),
-                                                   confidence_score_low=format(confidence_score_low, '.4g'),
-                                                   confidence_score_up=format(confidence_score_up, '.4g')
+                                   metric_name=metric_name,
+                                   value=float(format(overall_value, '.4g')),
+                                   confidence_score_low=float(format(confidence_score_low, '.4g')),
+                                   confidence_score_up=float(format(confidence_score_up, '.4g')),
             )
-
             if self._info.results.overall == None:
-                self._info.results.overall = [overall_performance]
+                self._info.results.overall = {}
+                self._info.results.overall[metric_name] = overall_performance
             else:
-                self._info.results.overall.append(overall_performance)
+                self._info.results.overall[metric_name] = overall_performance
 
 
     def _bucketing_samples(self, sysout_iterator):
