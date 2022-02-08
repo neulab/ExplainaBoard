b39f1355ed1081460da22f2f025776b0c3355b64
Pengfei Liu <59123869+pfliu-nlp@users.noreply.github.com>
2022-02-08 07:37:06 -0500

    fix print format issue
    Former-commit-id: 72547b323ca83af7448bcf6de63a15aa74d336b6

--- explainaboard/builders/kg_link_tail_prediction.py
+++ explainaboard/builders/kg_link_tail_prediction.py
@@ -87,15 +87,16 @@ class KGLTPExplainaboardBuilder:
             confidence_score_up = overall_value_json["confidence_score_up"]
             overall_performance = Performance(
                                                    metric_name=metric_name,
-                                                   value=format(overall_value, '.4g'),
-                                                   confidence_score_low=format(confidence_score_low, '.4g'),
-                                                   confidence_score_up=format(confidence_score_up, '.4g')
+                                                   value=float(format(overall_value, '.4g')),
+                                                   confidence_score_low=float(format(confidence_score_low, '.4g')),
+                                                   confidence_score_up=float(format(confidence_score_up, '.4g')),
             )
 
             if self._info.results.overall == None:
-                self._info.results.overall = [overall_performance]
+                self._info.results.overall = {}
+                self._info.results.overall[metric_name] = overall_performance
             else:
-                self._info.results.overall.append(overall_performance)
+                self._info.results.overall[metric_name] = overall_performance
 
     def _bucketing_samples(self, sysout_iterator):
 
