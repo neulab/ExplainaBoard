cfd23f356357933372b3e88c9d590508e01abc85
Pengfei Liu <59123869+pfliu-nlp@users.noreply.github.com>
2022-03-07 19:02:11 -0500

    add training set dependent features for extractive qa
    Former-commit-id: 99d64e02abf309dd0130fc89572ff5ec0393d853

--- explainaboard/builders/extractive_qa.py
+++ explainaboard/builders/extractive_qa.py
@@ -7,11 +7,61 @@ from explainaboard.utils.eval_basic_qa import *  # noqa
 from explainaboard.metric import *  # noqa
 from tqdm import tqdm
 
+
+from typing import Iterator, Dict, List
+from datalabs import load_dataset
+from datalabs.operations.aggregate.qa_extractive import qa_extractive_aggregating
+
 from eaas import Config, Client
 
 config = Config()
 client = Client()
 client.load_config(config)
+
+
+
+
+
+@qa_extractive_aggregating(name="get_statistics", contributor="datalab",
+                                 task="qa-extractive",
+                                 description="Calculate the overall statistics (e.g., average length) of "
+                                             "a given text classification dataset")
+def get_statistics(samples: Iterator):
+    """
+    Input:
+    samples: [{
+     "id":str
+     "context":str
+     "question":str
+     "answers":Dict
+     "options"
+    }]
+    """
+
+    vocab = {}
+    length_fre = {}
+    for sample in tqdm(samples):
+        context, answers = sample["context"], sample["answers"]
+
+
+        # update vocabulary
+        for w in context.split(" "):
+            if w in vocab.keys():
+                vocab[w] += 1
+            else:
+                vocab[w] = 1
+
+    # the rank of each word based on its frequency
+    sorted_dict = {key: rank for rank, key in enumerate(sorted(set(vocab.values()), reverse=True), 1)}
+    vocab_rank = {k: sorted_dict[v] for k, v in vocab.items()}
+
+    # print(vocab)
+    # print(vocab_rank)
+    # exit()
+
+    return {"vocab":vocab,
+            "vocab_rank":vocab_rank,
+            }
 
 
 
@@ -33,6 +83,25 @@ class QAExtractiveExplainaboardBuilder:
         self._samples_over_bucket = {}
         # _performances_over_bucket: performance in different bucket: Dict(feature_name, bucket_name, performance)
         self._performances_over_bucket = {}
+
+        # Calculate statistics of training set
+        self.statistics = None
+        if None != self._info.dataset_name:
+            try:
+                dataset = load_dataset(self._info.dataset_name, self._info.sub_dataset_name)
+                if "train" not in dataset.keys():
+                    self.statistics = None
+                elif len(dataset['train']._stat) == 0 or self._info.reload_stat == False: # calculate the statistics (_stat) when _stat is {} or `reload_stat` is False
+                    new_train = dataset['train'].apply(get_statistics, mode = "local")
+                    self.statistics = new_train._stat
+                else:
+                    self.statistics = dataset["train"]._stat
+            except FileNotFoundError as err:
+                eprint(
+                    "The dataset hasn't been supported by DataLab so no training set dependent features will be supported by ExplainaBoard."
+                    "You can add the dataset by: https://github.com/ExpressAI/DataLab/blob/main/docs/SDK/add_new_datasets_into_sdk.md")
+
+
 
     @staticmethod
     def get_bucket_feature_value(feature_name: str):
@@ -59,6 +128,32 @@ class QAExtractiveExplainaboardBuilder:
         res_json = client.bleu([[references]], [hypothesis], lang="en")
         return res_json["corpus_bleu"]
 
+
+    # training set dependent features
+    def _get_num_oov(self, existing_features: dict):
+        num_oov = 0
+
+        for w in existing_features["context"].split(" "):
+            if w not in self.statistics['vocab'].keys():
+                num_oov += 1
+        # print(num_oov)
+        return num_oov
+
+
+    # training set dependent features (this could be merged into the above one for further optimization)
+    def _get_fre_rank(self, existing_features: dict):
+        fre_rank = 0
+
+        for w in existing_features["context"].split(" "):
+            if w not in self.statistics['vocab_rank'].keys():
+                fre_rank += len(self.statistics['vocab_rank'])
+            else:
+                fre_rank += self.statistics['vocab_rank'][w]
+
+        fre_rank = fre_rank * 1.0 / len(existing_features["context"].split(" "))
+        return fre_rank
+
+
     def _complete_feature(self):
         """
         This function is used to calculate features used for bucekting, such as sentence_length
@@ -73,6 +168,16 @@ class QAExtractiveExplainaboardBuilder:
         ):
             # Get values of bucketing features
             for bucket_feature in bucket_features:
+
+                # this is need due to `del self._info.features[bucket_feature]`
+                if bucket_feature not in self._info.features.keys():
+                    continue
+                # If there is a training set dependent feature while no pre-computed statistics for it,
+                # then skip bucketing along this feature
+                if self._info.features[bucket_feature].require_training_set and self.statistics == None:
+                    del self._info.features[bucket_feature]
+                    continue
+
                 feature_value = eval(
                     QAExtractiveExplainaboardBuilder.get_bucket_feature_value(bucket_feature)
                 )(dict_sysout)
@@ -169,23 +274,26 @@ class QAExtractiveExplainaboardBuilder:
 
             for sample_id in sample_ids:
 
+
                 true_label = self._data[int(sample_id)]["answers"]["text"]
+                if isinstance(true_label, list):
+                    true_label = true_label[0]
 
                 predicted_label = self._data[int(sample_id)]["predicted_answers"]["text"]
                 sent = self._data[int(sample_id)]["question"]  # noqa
+                s_id = self._data[int(sample_id)]["id"]
 
                 # get a bucket of true/predicted labels
                 bucket_true_labels.append(true_label)
                 bucket_predicted_labels.append(predicted_label)
                 # get a bucket of cases (e.g., errors)
                 if self._info.results.is_print_case:
-                    if true_label[0] != predicted_label:
+                    if true_label != predicted_label:
                         # bucket_case = true_label[0] + "|||" + predicted_label + "|||" + sent
                         # bucket_case = {"true_answer": (sample_id, ["true_answers","text"]),
                         #                "predicted_answer": (sample_id, ["predicted_answer"]),
                         #                "question": (sample_id, ["question"])}
-                        system_output_id = self._data[int(sample_id)]["id"]
-                        bucket_case = system_output_id
+                        bucket_case = str(s_id)
                         bucket_cases.append(bucket_case)
 
             bucket_name_to_performance[bucket_interval] = []
