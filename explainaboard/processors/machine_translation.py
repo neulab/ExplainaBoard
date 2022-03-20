7b90d55c51d064a628a94d41217c2cd356dc36c9
Pengfei Liu <59123869+pfliu-nlp@users.noreply.github.com>
2022-03-20 16:25:16 -0400

    black formating
    Former-commit-id: c5d5502d3f0d0f6135529935ae45cd2e3244f72c

--- explainaboard/processors/machine_translation.py
+++ explainaboard/processors/machine_translation.py
@@ -13,7 +13,6 @@ from explainaboard.utils.py_utils import sort_dict
 from .conditional_generation import ConditionalGenerationProcessor
 
 
-
 @register_processor(TaskType.machine_translation)
 class MachineTranslationProcessor(ConditionalGenerationProcessor):
     _task_type = TaskType.machine_translation
@@ -22,22 +21,25 @@ class MachineTranslationProcessor(ConditionalGenerationProcessor):
     def __init__(self):
         super().__init__()
         # Inherit features from parent class and add new child-specific features
-        _features = super()._features.update(feature.Features(
-            {
-                # declaim task-specific features
-                "attr_compression": feature.Value(
-                    dtype="float",
-                    description="the ratio between source and reference length",
-                    is_bucket=True,
-                    bucket_info=feature.BucketInfo(
-                        method="bucket_attribute_specified_bucket_value",
-                        number=4,
-                        setting=(),
+        _features = super()._features.update(
+            feature.Features(
+                {
+                    # declaim task-specific features
+                    "attr_compression": feature.Value(
+                        dtype="float",
+                        description="the ratio between source and reference length",
+                        is_bucket=True,
+                        bucket_info=feature.BucketInfo(
+                            method="bucket_attribute_specified_bucket_value",
+                            number=4,
+                            setting=(),
+                        ),
                     ),
-                ),
-            }
-        ))
+                }
+            )
+        )
 
     def _get_attr_compression(self, existing_features: dict):
-        return len(existing_features["source"].split(" "))/len(existing_features["reference"].split(" "))
-
+        return len(existing_features["source"].split(" ")) / len(
+            existing_features["reference"].split(" ")
+        )
