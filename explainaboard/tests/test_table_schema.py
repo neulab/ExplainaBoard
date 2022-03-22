809b9883ee8fc0364d33dbdc011c7adea6d53b58
Graham Neubig <neubig@gmail.com>
2022-03-22 06:24:48 -0400

    Re-added table_schema

---
+++ explainaboard/tests/test_table_schema.py
@@ -0,0 +1,24 @@
+import unittest
+from explainaboard.table_schema import table_schemas
+from explainaboard.tasks import TaskType
+
+
+class TestTableSchema(unittest.TestCase):
+    def test_table_schemas(self):
+
+        print(table_schemas[TaskType.text_classification])
+
+        self.assertEqual(len(table_schemas[TaskType.text_classification]), 3)
+
+        print(table_schemas[TaskType.summarization])
+        self.assertEqual(len(table_schemas[TaskType.summarization]), 3)
+
+        print(table_schemas[TaskType.question_answering_extractive])
+        self.assertEqual(len(table_schemas[TaskType.question_answering_extractive]), 4)
+
+        print(table_schemas[TaskType.named_entity_recognition])
+        self.assertEqual(len(table_schemas[TaskType.named_entity_recognition]), 4)
+
+
+if __name__ == '__main__':
+    unittest.main()
