5f520f9e0bebca7157021639eecdfe1bf36adb08
Pengfei Liu <stefanpengfei@gmail.com>
2022-03-23 02:38:19 -0400

    update datalabs>=0.3.7 since current version relies on the latest DataLab

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
         "datasets",
         "lexicalrichness",
         "spacy",
-        "datalabs>=0.3.6",
+        "datalabs>=0.3.7",
     ],
     extras_require={"dev": ["black", "flake8", "pre-commit"]},
     include_package_data=True,
