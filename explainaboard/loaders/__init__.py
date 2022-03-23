4b93b9542b714754eb91d718cd82b98ab706d11c
Lyuyang Hu <lyuyangh@andrew.cmu.edu>
2022-03-23 11:16:51 -0400

    remove hellaswag

--- explainaboard/loaders/__init__.py
+++ explainaboard/loaders/__init__.py
@@ -6,7 +6,6 @@ from . import named_entity_recognition
 from . import extractive_qa
 from . import conditional_generation
 from . import text_pair_classification
-from . import hellaswag
 from . import aspect_based_sentiment_classification
 from . import kg_link_tail_prediction
 from . import qa_multiple_choice
