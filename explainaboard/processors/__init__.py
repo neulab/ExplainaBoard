bc896eaf5bad70e6442ecaa96d1c28c96d7296a9
odashi <yus.takara@gmail.com>
2022-03-26 15:07:35 +0000

    Remove all noqa.
    Former-commit-id: ef975d9b04cd182dd44a8f57c346ae11b079c4ce

--- explainaboard/processors/__init__.py
+++ explainaboard/processors/__init__.py
@@ -1,16 +1,26 @@
-# flake8: noqa
 # when a new processor is implemented, remember to import it here so it gets registered
-from . import text_classification
-from . import named_entity_recognition
-from . import extractive_qa
-from . import conditional_generation
-from . import summarization
-from . import machine_translation
+from explainaboard.processors import aspect_based_sentiment_classification
+from explainaboard.processors import conditional_generation
+from explainaboard.processors import extractive_qa
+from explainaboard.processors import kg_link_tail_prediction
+from explainaboard.processors import machine_translation
+from explainaboard.processors import named_entity_recognition
+from explainaboard.processors import qa_multiple_choice
+from explainaboard.processors import summarization
+from explainaboard.processors import text_classification
+from explainaboard.processors import text_pair_classification
+from explainaboard.processors.processor_registry import get_processor
 
-from . import text_pair_classification
-
-from . import aspect_based_sentiment_classification
-
-from . import kg_link_tail_prediction
-from . import qa_multiple_choice
-from .processor_registry import get_processor
+__all__ = [
+    'aspect_based_sentiment_classification',
+    'conditional_generation',
+    'extractive_qa',
+    'get_processor',
+    'kg_link_tail_prediction',
+    'machine_translation',
+    'named_entity_recognition',
+    'qa_multiple_choice',
+    'summarization',
+    'text_classification',
+    'text_pair_classification',
+]
