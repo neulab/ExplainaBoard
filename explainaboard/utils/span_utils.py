17d355a00224cfbfd731165b0ad00a2fa27c4198
Graham Neubig <neubig@gmail.com>
2022-04-02 09:17:49 -0400

    Fixed documentation and bumped version
    Former-commit-id: d909e7e01306bb2f6fd69a6aaf5b785e30400114

--- explainaboard/utils/span_utils.py
+++ explainaboard/utils/span_utils.py
@@ -1,30 +1,25 @@
 from __future__ import annotations
 
 
-def get_spans_from_bio(seq):
+def get_spans_from_bio(bio_seq: list[str]) -> list[tuple[str, int, int]]:
     """
-    tags:dic{'per':1,....}
-    Args:
-        seq: [4, 4, 0, 0, ...] sequence of labels
-        tags: dict["O"] = 4
-    Returns:
-        list of (span_type, span_start, span_end)
-    Example:
-        seq = [4, 5, 0, 3]
-        tags = {"B-PER": 4, "I-PER": 5, "B-LOC": 3}
-        result = [("PER", 0, 2), ("LOC", 3, 4)]
+    Takes in a BIO-tagged sequence of tokens, and returns tagged spans.
+    :param bio_seq: A sequence of bio-tagged strings
+                    such as ['O', 'B-PER', 'I-PER', 'B-ORG', 'O']
+    :return: A sequence of spans in format (tag,begin,end),
+             such as [('PER',1,3), ('ORG',3,4)]
     """
     default = 'O'
     # idx_to_tag = {idx: tag for tag, idx in tags.items()}
     spans = []
-    span_type, span_start = None, None
-    for i, tok in enumerate(seq):
+    span_type, span_start = None, -1
+    for i, tok in enumerate(bio_seq):
         # End of a span 1
         if tok == default and span_type is not None:
             # Add a span.
             span = (span_type, span_start, i)
             spans.append(span)
-            span_type, span_start = None, None
+            span_type, span_start = None, -1
 
         # End of a span + start of a span!
         elif tok != default:
@@ -39,7 +34,7 @@ def get_spans_from_bio(seq):
             pass
     # end condition
     if span_type is not None:
-        span = (span_type, span_start, len(seq))
+        span = (span_type, span_start, len(bio_seq))
         spans.append(span)
 
     return spans
