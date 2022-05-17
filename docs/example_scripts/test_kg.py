e694aed6a0b6c9ebf1b2e88fdaab363e6ca4d8ba
Pengfei Liu <stefanpengfei@gmail.com>
2022-05-17 11:42:29 -0400

    update

--- docs/example_scripts/test_kg.py
+++ docs/example_scripts/test_kg.py
@@ -1,7 +1,9 @@
-from explainaboard import TaskType, get_custom_dataset_loader, get_processor
+from explainaboard import get_custom_dataset_loader, get_processor, TaskType
 
 # Load the data
-dataset = "./explainaboard/tests/artifacts/kg_link_tail_prediction/no_custom_feature.json"
+dataset = (
+    "./explainaboard/tests/artifacts/kg_link_tail_prediction/no_custom_feature.json"
+)
 task = TaskType.kg_link_tail_prediction
 loader = get_custom_dataset_loader(task, dataset, dataset)
 data = loader.load()
@@ -11,28 +13,30 @@ sys_info = processor.process(metadata={}, sys_output=data.samples)
 
 
 # print bucket information
-processor.print_bucket_info(sys_info.results.fine_grained)
+processor.print_bucket_info(sys_info.results.fine_grained)  # type: ignore
 
 # save analysis report locally
 sys_info.print_as_json(file=open("./report.json", 'w'))
 
 
 # get overall results of different metrics
-for metric_name, metric_info in sys_info.results.overall.items():
+for metric_name, metric_info in sys_info.results.overall.items():  # type: ignore
 
     metric_name = metric_info.metric_name
     value = metric_info.value
     confidence_score_low = metric_info.confidence_score_low
     confidence_score_high = metric_info.confidence_score_high
 
-    print(f"metric_name:{metric_name}\n"
-          f"value:{value }\n"
-          f"confidence_score_low:{confidence_score_low}\n"
-          f"confidence_score_high:{confidence_score_high}\n")
+    print(
+        f"metric_name:{metric_name}\n"
+        f"value:{value }\n"
+        f"confidence_score_low:{confidence_score_low}\n"
+        f"confidence_score_high:{confidence_score_high}\n"
+    )
 
 
 # get fine-grained results
-for feature_name, feature_info in sys_info.results.fine_grained.items():
+for feature_name, feature_info in sys_info.results.fine_grained.items():  # type: ignore
     for bucket_name, bucket_info in feature_info.items():
         bucket_n_samples = bucket_info.n_samples
         for bucket_performance in bucket_info.performances:
@@ -43,8 +47,9 @@ for feature_name, feature_info in sys_info.results.fine_grained.items():
 
             print("------------------------------------------------------")
             print(f"feature_name:{feature_name} bucket_name:{bucket_name}")
-            print(f"metric_name:{metric_name}\n"
-                  f"value:{value}\n"
-                  f"confidence_score_low:{confidence_score_low}\n"
-                  f"confidence_score_high:{confidence_score_high}\n")
-
+            print(
+                f"metric_name:{metric_name}\n"
+                f"value:{value}\n"
+                f"confidence_score_low:{confidence_score_low}\n"
+                f"confidence_score_high:{confidence_score_high}\n"
+            )
