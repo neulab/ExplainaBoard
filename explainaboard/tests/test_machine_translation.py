ccb4f2666f8255fc964d043a7f688b1a899f7826
Graham Neubig <neubig@gmail.com>
2022-05-19 08:55:54 -0400

    Add tests

--- explainaboard/tests/test_machine_translation.py
+++ explainaboard/tests/test_machine_translation.py
@@ -10,6 +10,7 @@ class TestMachineTranslation(unittest.TestCase):
     artifact_path = os.path.join(test_artifacts_path, "machine_translation")
     tsv_dataset = os.path.join(artifact_path, "dataset.tsv")
     txt_output = os.path.join(artifact_path, "output.txt")
+    json_output_with_features = os.path.join(artifact_path, "output_with_features.json")
 
     def test_load_tsv(self):
         loader = get_custom_dataset_loader(
@@ -74,6 +75,31 @@ class TestMachineTranslation(unittest.TestCase):
         self.assertDictEqual(condgen_features_1, condgen_features_2)
         # condgen features are a subset of MT features
         self.assertDictEqual(mt_features, {**mt_features, **condgen_features_1})
+
+    def test_custom_features(self):
+        loader = get_custom_dataset_loader(
+            TaskType.machine_translation,
+            self.tsv_dataset,
+            self.json_output_with_features,
+            Source.local_filesystem,
+            Source.local_filesystem,
+            FileType.tsv,
+            FileType.json,
+        )
+        data = loader.load()
+        self.assertEqual(len(data), 4)
+        self.assertEqual(
+            data[0],
+            {
+                'source': 'Ak sa chcete dostať ešte hlbšie, môžete si všimnúť '
+                + 'trhlinky.',
+                'reference': 'Now just to get really deep in , you can really get to '
+                + 'the cracks .',
+                'id': '0',
+                'hypothesis': 'If you want to get a deeper , you can see the forces .',
+                'num_capital_letters': 1,
+            },
+        )
 
 
 if __name__ == '__main__':
