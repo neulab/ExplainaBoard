5d2581773b8c6daffc36abce71a78aa94adc1fce
Graham Neubig <neubig@gmail.com>
2022-05-23 03:26:47 +0100

    More aggressive serialization

--- explainaboard/utils/serialization.py
+++ explainaboard/utils/serialization.py
@@ -1,9 +1,16 @@
 import copy
+import dataclasses
 
 
 def general_to_dict(data):
     if hasattr(data, 'to_dict'):
         return getattr(data, 'to_dict')()
+    elif dataclasses.is_dataclass(data):
+        return dataclasses.asdict(data, dict_factory=explainaboard_dict_factory)
+    elif isinstance(data, dict):
+        return {k: general_to_dict(v) for k, v in data.items()}
+    elif isinstance(data, list):
+        return [general_to_dict(v) for v in data]
     else:
         return copy.deepcopy(data)
 
