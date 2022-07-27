480fa7d04e12a68ca1e48e5b0049deb292bfe0a8
Pengfei Liu <stefanpengfei@gmail.com>
2022-07-27 00:37:26 -0400

    add multiple-annotator features to metric config

--- explainaboard/metrics/human_eval.py
+++ explainaboard/metrics/human_eval.py
@@ -10,10 +10,13 @@ from explainaboard.metrics.registry import register_metric_config
 
 HUMAN_METRICS = ["LikertScore_fluency", "LikertScore_coherence"]
 
+
 @dataclass
 @register_metric_config
 class LikertScoreConfig(MetricConfig):
-    aspect:str = "fluency"
+    aspect: str = "fluency"
+    n_annotators: int = 3
+    agreement: float = 0.0
 
     def to_metric(self):
         return LikertScore(self)
@@ -25,14 +28,31 @@ class LikertScore(Metric):
     outputs.
     """
 
+    def is_simple_average(self, stats: MetricStats):
+        return False
+
     def calc_stats_from_data(
         self, true_data: list, pred_data: list, config: Optional[MetricConfig] = None
     ) -> MetricStats:
         config = cast(LikertScoreConfig, self._get_config(config))
+
+        # TODO(Calculate Agreement)
+
+        # "-1" indicates samples to be evaluated
         return MetricStats(
             np.array(
-                [
-                    -1.0 for t, p in zip(true_data, pred_data)
-                ]
+                [[-1.0] * config.n_annotators for t, p in zip(true_data, pred_data)]
             )
         )
+
+    def aggregate_stats(self, stats: MetricStats) -> np.ndarray:
+        """
+        Aggregate sufficient statistics from multiple examples into a single example
+        :param stats: stats for every example
+        :return: aggregated stats
+        """
+        data = stats.get_data()
+        if data.size == 0:
+            return np.array(0.0)
+        else:
+            return np.mean(np.mean(data, axis=-1), axis=-1)  # this could be redefined
