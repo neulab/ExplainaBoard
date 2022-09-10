0711126ba6b7ce60ee8bd0045fdaa191928bf44f
Shuaichen Chang <shuaichenchang@users.noreply.github.com>
2022-09-10 14:04:10 -0400

    fix format

--- explainaboard/loaders/text_to_sql.py
+++ explainaboard/loaders/text_to_sql.py
@@ -2,8 +2,7 @@ from __future__ import annotations
 
 from explainaboard import TaskType
 from explainaboard.constants import FileType
-from explainaboard.loaders.file_loader import (
-    DatalabFileLoader,
+from explainaboard.loaders.file_loader import (  # DatalabFileLoader,
     FileLoader,
     FileLoaderField,
     JSONFileLoader,
