0817722a761e97e91825246a78e14b3f18b6b006
Pengfei Liu <stefanpengfei@gmail.com>
2022-12-27 16:29:07 -0500

    add tests for meval to replicate paper results

--- integration_tests/meta_eval_nlg_test.py
+++ integration_tests/meta_eval_nlg_test.py
@@ -1,20 +1,24 @@
 from __future__ import annotations
 
+import os
 import unittest
 
+from integration_tests.utils import test_artifacts_path
 import numpy as np
 
+from explainaboard import FileType, get_processor_class, Source, TaskType
+from explainaboard.loaders.file_loader import DatalabLoaderOption
+from explainaboard.loaders.loader_factory import get_loader_class
 from explainaboard.metrics.meta_evaluation import CorrelationNLG, CorrelationNLGConfig
+from explainaboard.metrics.metric import Score
 from explainaboard.utils.typing_utils import narrow, unwrap
 
 
 class MetaEvalNLGInvalidValueTest(unittest.TestCase):
-
     true_data = [[1, 2, 3, 4, 5], [2, 1, 4, 5, 2], [5, 4, 3, 2, 1]]
     pred_data = [[2, 1, 3, 4, 5], [2, 4, 5, 5, 2], [5, 3, 4, 2, 1]]
 
     def test_illegal_correlation_type_calc_stats_from_data(self) -> None:
-
         nlg_corr_config = CorrelationNLGConfig(
             group_by="sample", correlation_type="illegal"
         )
@@ -33,7 +37,6 @@ class MetaEvalNLGInvalidValueTest(unittest.TestCase):
             corr_metric._calc_metric_from_aggregate_single(stats_arr)
 
     def test_illegal_group_type_calc_stats_from_data(self) -> None:
-
         nlg_corr_config = CorrelationNLGConfig(
             group_by="illegal", correlation_type="spearmanr"
         )
@@ -61,7 +64,6 @@ class MetaEvalNLGTest(unittest.TestCase):
     pred_data = [[2, 1, 3, 4, 5], [2, 4, 5, 5, 2], [5, 3, 4, 2, 1]]
 
     def test_sample_level_spearmanr(self) -> None:
-
         nlg_corr_config = CorrelationNLGConfig(
             group_by="sample", correlation_type="spearmanr"
         )
@@ -73,7 +75,6 @@ class MetaEvalNLGTest(unittest.TestCase):
         self.assertAlmostEqual(val, 0.8162952, 3)
 
     def test_sample_level_kendalltau(self) -> None:
-
         nlg_corr_config = CorrelationNLGConfig(
             group_by="sample", correlation_type="kendalltau"
         )
@@ -84,7 +85,6 @@ class MetaEvalNLGTest(unittest.TestCase):
         self.assertAlmostEqual(val, 0.69046817, 3)
 
     def test_sample_level_pearsonr(self) -> None:
-
         nlg_corr_config = CorrelationNLGConfig(
             group_by="sample", correlation_type="pearsonr"
         )
@@ -95,7 +95,6 @@ class MetaEvalNLGTest(unittest.TestCase):
         self.assertAlmostEqual(val, 0.820707397, 3)
 
     def test_system_level_spearmanr(self) -> None:
-
         nlg_corr_config = CorrelationNLGConfig(
             group_by="system", correlation_type="spearmanr"
         )
@@ -106,7 +105,6 @@ class MetaEvalNLGTest(unittest.TestCase):
         self.assertAlmostEqual(val, 0.815789, 3)
 
     def test_system_level_kendalltau(self) -> None:
-
         nlg_corr_config = CorrelationNLGConfig(
             group_by="system", correlation_type="kendalltau"
         )
@@ -117,7 +115,6 @@ class MetaEvalNLGTest(unittest.TestCase):
         self.assertAlmostEqual(val, 0.66666, 3)
 
     def test_dataset_level_spearmanr(self) -> None:
-
         true_data = [[1], [2], [3], [4], [5]]
         pred_data = [[1], [2], [3], [4], [5]]
 
@@ -132,12 +129,10 @@ class MetaEvalNLGTest(unittest.TestCase):
 
 
 class MetaEvalNLGCITest(unittest.TestCase):
-
     true_data = [[1, 2, 3, 4, 5], [2, 1, 4, 5, 2], [5, 4, 3, 2, 1]]
     pred_data = [[2, 1, 3, 4, 5], [2, 4, 5, 5, 2], [5, 3, 4, 2, 1]]
 
     def test_sample_level_spearmanr_bootstrap(self) -> None:
-
         nlg_corr_config = CorrelationNLGConfig(
             group_by="sample", correlation_type="spearmanr"
         )
@@ -154,7 +149,6 @@ class MetaEvalNLGCITest(unittest.TestCase):
         self.assertAlmostEqual(ci[1], 0.8999, 2)
 
     def test_system_level_spearmanr_bootstrap(self) -> None:
-
         nlg_corr_config = CorrelationNLGConfig(
             group_by="system", correlation_type="spearmanr"
         )
@@ -171,7 +165,6 @@ class MetaEvalNLGCITest(unittest.TestCase):
         self.assertAlmostEqual(ci[1], 0.9746, 2)
 
     def test_dataset_level_spearmanr_bootstrap(self) -> None:
-
         true_data = [[1], [2], [3], [4], [5]]
         pred_data = [[1], [2], [3], [4], [5]]
 
@@ -189,3 +182,74 @@ class MetaEvalNLGCITest(unittest.TestCase):
         ci = unwrap(corr_metric.calc_confidence_interval(stats, 0.05))
         self.assertAlmostEqual(ci[0], 1, 2)
         self.assertAlmostEqual(ci[1], 1, 2)
+
+
+class MetaEvalNLGNewsroomTest(unittest.TestCase):
+    """
+    Test the NLG metric on newsroom dataset and replicate the reported results from
+    the paper: https://arxiv.org/pdf/2106.11520.pdf
+    """
+
+    artifact_path = os.path.join(test_artifacts_path, "newsroom")
+    predictions_rouge1 = os.path.join(artifact_path, "rouge1_f_predictions.json")
+    predictions_bartscore = os.path.join(
+        artifact_path, "bart_score_cnn_ref_hypo_predictions.json"
+    )
+
+    def test_coherence_rouge1_f(self):
+        loader = get_loader_class(TaskType.meta_evaluation_nlg).from_datalab(
+            dataset=DatalabLoaderOption("meval_newsroom", "coherence"),
+            output_data=self.predictions_rouge1,
+            output_source=Source.local_filesystem,
+            output_file_type=FileType.json,
+        )
+        data = loader.load().samples
+
+        metadata = {
+            "task_name": TaskType.meta_evaluation_nlg.value,
+            "dataset_name": "meval_newsroom",
+            "sub_dataset_name": "coherence",
+            "metric_names": ["SpearmanSampleLevelCorr"],
+        }
+        processor = get_processor_class(TaskType.meta_evaluation_nlg)()
+        sys_info = processor.process(metadata, data)
+        overall_score = (
+            sys_info.results.overall["example"]["SpearmanSampleLevelCorr"]
+            .get_value(Score, "score")
+            .value
+        )
+        self.assertGreater(len(sys_info.results.analyses), 0)
+        self.assertAlmostEqual(
+            overall_score,
+            0.0946,
+            places=3,
+        )
+
+    def test_coherence_bartscore(self):
+        loader = get_loader_class(TaskType.meta_evaluation_nlg).from_datalab(
+            dataset=DatalabLoaderOption("meval_newsroom", "coherence"),
+            output_data=self.predictions_bartscore,
+            output_source=Source.local_filesystem,
+            output_file_type=FileType.json,
+        )
+        data = loader.load().samples
+
+        metadata = {
+            "task_name": TaskType.meta_evaluation_nlg.value,
+            "dataset_name": "meval_newsroom",
+            "sub_dataset_name": "coherence",
+            "metric_names": ["SpearmanSampleLevelCorr"],
+        }
+        processor = get_processor_class(TaskType.meta_evaluation_nlg)()
+        sys_info = processor.process(metadata, data)
+        overall_score = (
+            sys_info.results.overall["example"]["SpearmanSampleLevelCorr"]
+            .get_value(Score, "score")
+            .value
+        )
+        self.assertGreater(len(sys_info.results.analyses), 0)
+        self.assertAlmostEqual(
+            overall_score,
+            0.3157,
+            places=3,
+        )
