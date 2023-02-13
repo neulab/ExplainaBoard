8d9d7c6ebd8157e91017a05ffe9e27a3dfbb885e
Graham Neubig <neubig@gmail.com>
2023-02-13 15:29:54 +0000

    Add more tests

--- explainaboard/metrics/continuous_test.py
+++ explainaboard/metrics/continuous_test.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+import math
 import unittest
 
 from explainaboard.metrics.continuous import (
@@ -38,6 +39,28 @@ class RootMeanSquaredErrorConfigTest(unittest.TestCase):
         )
 
 
+class RootMeanSquaredErrorTest(unittest.TestCase):
+    def test_basic_calculation(self) -> None:
+        absolute_error = narrow(
+            RootMeanSquaredError, RootMeanSquaredErrorConfig().to_metric()
+        )
+        expected = [1.0, 3.0]
+        actual = [1.4, 2.8]
+        metric_result = absolute_error.evaluate(expected, actual)
+        value = metric_result.get_value(Score, "score").value
+        self.assertAlmostEqual(value, math.sqrt(0.1))
+
+    def test_negative(self) -> None:
+        absolute_error = narrow(
+            RootMeanSquaredError, RootMeanSquaredErrorConfig(negative=True).to_metric()
+        )
+        expected = [1.0, 3.0]
+        actual = [1.4, 2.8]
+        metric_result = absolute_error.evaluate(expected, actual)
+        value = metric_result.get_value(Score, "score").value
+        self.assertAlmostEqual(value, -math.sqrt(0.1))
+
+
 class AbsoluteErrorConfigTest(unittest.TestCase):
     def test_serialize(self) -> None:
         self.assertEqual(
@@ -63,7 +86,6 @@ class AbsoluteErrorConfigTest(unittest.TestCase):
 
 
 class AbsoluteErrorTest(unittest.TestCase):
-
     def test_basic_calculation(self) -> None:
         absolute_error = narrow(AbsoluteError, AbsoluteErrorConfig().to_metric())
         expected = [1.0, 2.0, 3.0]
@@ -71,3 +93,13 @@ class AbsoluteErrorTest(unittest.TestCase):
         metric_result = absolute_error.evaluate(expected, actual)
         value = metric_result.get_value(Score, "score").value
         self.assertAlmostEqual(value, 0.4)
+
+    def test_negative(self) -> None:
+        absolute_error = narrow(
+            AbsoluteError, AbsoluteErrorConfig(negative=True).to_metric()
+        )
+        expected = [1.0, 2.0, 3.0]
+        actual = [1.5, 2.0, 3.7]
+        metric_result = absolute_error.evaluate(expected, actual)
+        value = metric_result.get_value(Score, "score").value
+        self.assertAlmostEqual(value, -0.4)
